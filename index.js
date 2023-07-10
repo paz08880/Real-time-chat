cf9b07431774ffdae08bc86fd317e91791961fe4
Daniel Fuzailove <47007111+paz08880@users.noreply.github.com>
2023-07-10 19:26:27 +0300

    Adding online users functions

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const io = require('socket.io')(3000)
-let users = {};
+const io = require('socket.io')(3000);
+const users = {};
 
-io.on("connection", socket => {
-
-    socket.on("new-user", name => {
+io.on("connection", (socket) => {
+    socket.on("new-user", (name) => {
         users[socket.id] = name;
-        console.log(users);
         socket.broadcast.emit('user-connected', users[socket.id]);
-    })
+    });
 
-    socket.on("typing", boolean => {
-        if(boolean){
+    socket.on("typing", (boolean) => {
+        if (boolean) {
             socket.broadcast.emit("user-typing", `${users[socket.id]} is typing`);
-        }else{
+        } else {
             socket.broadcast.emit("user-typing", false);
         }
-    })
-    
-    socket.on("send-message", message => {
-        socket.broadcast.emit('send-target-chat', {message: message, name: users[socket.id]} )
-    })
+    });
 
+    socket.on("send-online-users", () => {
+        socket.emit("get-online-users", users);
+    });
+
+    socket.on("send-message", (message) => {
+        socket.broadcast.emit('send-target-chat', { message: message, name: users[socket.id] });
+    });
 
     socket.on("disconnect", () => {
         socket.broadcast.emit('user-left', users[socket.id]);
         delete users[socket.id];
-    })
-})
-
+    });
+});
--- src/script.js
+++ src/script.js
@@ -1,59 +1,69 @@
-const socket = io("http://localhost:3000/" ,{ transports: ['websocket', 'polling', 'flashsocket'] });
+const socket = io("http://localhost:3000/", { transports: ['websocket', 'polling', 'flashsocket'] });
 const formMessage = document.getElementById("form-message");
 let containerMessage = document.getElementById("app-container");
 let inputMessage = document.getElementById("message");
 let span = document.getElementById("isTyping");
+let onlineUsers = document.getElementById("onlineUsers");
 
-const inputName = prompt("enter your name");
-AddMessage("you joined");
+const inputName = prompt("Enter your name");
+AddMessage("You joined");
 socket.emit("new-user", inputName);
+
+setInterval(() => {
+    checkUsers();
+}, 1000);
 
 formMessage.addEventListener("submit", (e) => {
     e.preventDefault();
-    message = inputMessage.value;
-    if(message == "") return;
+    const message = inputMessage.value;
+    if (message === "") return;
     AddMessage(`You: ${message}`);
     socket.emit("send-message", message);
     socket.emit("typing", false);
     inputMessage.value = "";
-})
+});
 
-//check if user typing
-inputMessage.addEventListener('keydown', (e) => {
-    console.log(e.target.value);
-    if(inputMessage.value != "" && inputMessage.value.length >= 1){
+// Check if user is typing
+inputMessage.addEventListener('input', () => {
+    if (inputMessage.value !== "") {
         socket.emit("typing", true);
-    }else{
+    } else {
         socket.emit("typing", false);
     }
-})
+});
 
+socket.on("get-online-users", (users) => {
+    let firstNames = Object.values(users).map(name => name.split(' ')[0]);
+    onlineUsers.innerText = `${firstNames.length} online users`;
+});
 
+socket.on("user-connected", (name) => {
+    AddMessage(`${name} connected`);
+});
 
-socket.on("user-connected", name => {
-    AddMessage(name + " connected");
-})
-
-socket.on("user-typing", message => {
-    if(message == false){
+socket.on("user-typing", (message) => {
+    if (message === false) {
         span.innerText = "";
-    }else{
+    } else {
         span.innerText = message;
     }
-})
+});
 
-socket.on("user-left", name => {
-    AddMessage(name + "  disconnected");
-})
+socket.on("user-left", (name) => {
+    AddMessage(`${name} disconnected`);
+});
 
-socket.on("send-target-chat", data => {
-    AddMessage(`${data.name}: ${data.message}`)
-})
+socket.on("send-target-chat", (data) => {
+    AddMessage(`${data.name}: ${data.message}`);
+});
 
-
-function AddMessage(Message){
-    let el = document.createElement("div");
+function AddMessage(message) {
+    const el = document.createElement("div");
     el.setAttribute("id", "gotMessage");
-    el.innerText = Message;
+    el.innerText = message;
     containerMessage.append(el);
+}
+
+function checkUsers() {
+    socket.emit("send-online-users");
 }
